a8cb848590aa8749eaf2814a01f020582afbc372
Davlatov Shahzod <shahzod@davlatov.space>
2023-10-31 22:15:38 +0300

    Lecture 1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,8 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { TestBed } from "@angular/core/testing";
+import { RouterTestingModule } from "@angular/router/testing";
+import { AppComponent } from "./app.component";
 
-describe('AppComponent', () => {
+describe("AppComponent", () => {
     beforeEach(() =>
         TestBed.configureTestingModule({
             imports: [RouterTestingModule],
@@ -10,7 +10,7 @@ describe('AppComponent', () => {
         }),
     );
 
-    it('should create the app', () => {
+    it("should create the app", () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.componentInstance;
         expect(app).toBeTruthy();
@@ -19,15 +19,15 @@ describe('AppComponent', () => {
     it(`should have as title 'Angular-learnjs-261023'`, () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.componentInstance;
-        expect(app.title).toEqual('Angular-learnjs-261023');
+        expect(app.title).toEqual("Angular-learnjs-261023");
     });
 
-    it('should render title', () => {
+    it("should render title", () => {
         const fixture = TestBed.createComponent(AppComponent);
         fixture.detectChanges();
         const compiled = fixture.nativeElement as HTMLElement;
-        expect(compiled.querySelector('.content span')?.textContent).toContain(
-            'Angular-learnjs-261023 app is running!',
+        expect(compiled.querySelector(".content span")?.textContent).toContain(
+            "Angular-learnjs-261023 app is running!",
         );
     });
 });
