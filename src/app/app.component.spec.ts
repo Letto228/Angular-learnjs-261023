9b7f9817aeecf7a5141e19821adbc90b965903ea
danterep <antipoff7@yandex.ru>
2023-11-01 14:31:08 +0200

    feat(eslint-config): Airbnb style guide, imports auto sorting, unused imports removal

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,33 +1,32 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { TestBed } from "@angular/core/testing";
+import { RouterTestingModule } from "@angular/router/testing";
 
-describe('AppComponent', () => {
-    beforeEach(() =>
-        TestBed.configureTestingModule({
-            imports: [RouterTestingModule],
-            declarations: [AppComponent],
-        }),
-    );
+import { AppComponent } from "./app.component";
 
-    it('should create the app', () => {
+describe("AppComponent", () => {
+    beforeEach(() => TestBed.configureTestingModule({
+        imports: [RouterTestingModule],
+        declarations: [AppComponent],
+    }),);
+
+    it("should create the app", () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.componentInstance;
         expect(app).toBeTruthy();
     });
 
-    it(`should have as title 'Angular-learnjs-261023'`, () => {
+    it("should have as title 'Angular-learnjs-261023'", () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.componentInstance;
-        expect(app.title).toEqual('Angular-learnjs-261023');
+        expect(app.title).toEqual("Angular-learnjs-261023");
     });
 
-    it('should render title', () => {
+    it("should render title", () => {
         const fixture = TestBed.createComponent(AppComponent);
         fixture.detectChanges();
         const compiled = fixture.nativeElement as HTMLElement;
-        expect(compiled.querySelector('.content span')?.textContent).toContain(
-            'Angular-learnjs-261023 app is running!',
+        expect(compiled.querySelector(".content span")?.textContent).toContain(
+            "Angular-learnjs-261023 app is running!",
         );
     });
 });
