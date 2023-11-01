e0dd3cc83e58566943d411a5a0829768cd4d44da
AlekseyDyakonov1992 <alexei.dyakonov@gmail.com>
2023-11-01 22:13:04 +0400

    feat: do some experiments with settings of eslint and prettier

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,12 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.isProd) {
-    // code
+  // code
+  let arg = 'test';
+  arg = arg.length ? 'test1' : 'test2'; // if used arg? then get no-unnecessary-condition error from eslint
+  console.error(arg); // just refuse from @typescript-eslint/no-unused-vars error
 }
 
 platformBrowserDynamic()
-    .bootstrapModule(AppModule)
-    .catch((err) => console.error(err));
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
