908b4d67c3bf562a7c04d723a36b2a997c94ae15
AlekseyDyakonov1992 <alexei.dyakonov@gmail.com>
2023-11-04 18:45:38 +0400

    feat: add friendship between prettier and eslint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  isProd: true,
+    isProd: true,
 };
--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,12 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.isProd) {
-  // code
-  let arg = 'test';
-  arg = arg.length ? 'test1' : 'test2'; // if used arg? then get no-unnecessary-condition error from eslint
-  console.error(arg); // just refuse from @typescript-eslint/no-unused-vars error
+    // code
+    let arg = 'test';
+    arg = arg.length ? 'test1' : 'test2'; // if used arg? then get no-unnecessary-condition error from eslint
+    console.error(arg); // just refuse from @typescript-eslint/no-unused-vars error
 }
 
 platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+    .bootstrapModule(AppModule)
+    .catch((err) => console.error(err));
