9c61c4d373d46f0b062ce7985686476ba234b4c7
OneScriptMan <vladzam79@gmail.com>
2023-11-07 19:48:06 +0300

    feat: add card planset

--- src/app/pages/products-list/card/card.component.ts
+++ src/app/pages/products-list/card/card.component.ts
@@ -1,8 +1,44 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {productMock} from '../../../shared/products/product.mock';
 
 @Component({
     selector: 'app-card',
     templateUrl: './card.component.html',
     styleUrls: ['./card.component.css'],
 })
-export class CardComponent {}
+export class CardComponent implements OnInit {
+    onClick(event: MouseEvent) {
+        // eslint-disable-next-line no-console
+        console.log('Button click');
+        event.stopPropagation();
+    }
+
+    product = productMock;
+    columns?: number;
+
+    // Cards
+    breakPoints() {
+        switch (true) {
+            case window.innerWidth <= 796:
+                this.columns = 1;
+                break;
+            case window.innerWidth > 797 && window.innerWidth <= 1233:
+                this.columns = 2;
+                break;
+            case window.innerWidth > 1234 && window.innerWidth <= 1597:
+                this.columns = 3;
+                break;
+            default:
+                this.columns = 4;
+        }
+    }
+
+    ngOnInit(): void {
+        // Contact list
+        this.breakPoints();
+    }
+    // eslint-disable-next-line
+    onResize(event: Event) {
+        this.breakPoints();
+    }
+}
--- src/app/pages/products-list/products-list.module.ts
+++ src/app/pages/products-list/products-list.module.ts
@@ -1,11 +1,15 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {ProductsListComponent} from './products-list.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatGridListModule} from '@angular/material/grid-list';
 import {CardComponent} from './card/card.component';
+import {ProductsListComponent} from './products-list.component';
 
 @NgModule({
     declarations: [ProductsListComponent, CardComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule, MatGridListModule],
     exports: [ProductsListComponent],
 })
 export class ProductsListModule {}
