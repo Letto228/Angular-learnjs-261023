226d9a598403ff1add1d07504ff35b89876e5568
OneScriptMan <vladzam79@gmail.com>
2023-11-10 11:41:29 +0300

    feat: add mat card

--- src/app/pages/products-list/products-list.module.ts
+++ src/app/pages/products-list/products-list.module.ts
@@ -1,11 +1,13 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 import {ProductsListComponent} from './products-list.component';
 import {CardComponent} from './card/card.component';
 
 @NgModule({
     declarations: [ProductsListComponent, CardComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, MatCardModule, MatButtonModule],
     exports: [ProductsListComponent],
 })
 export class ProductsListModule {}
