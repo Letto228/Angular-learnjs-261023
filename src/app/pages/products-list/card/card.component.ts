7dca65caca16d773beeb63e5ba9322495cacb50e
AlekseyDyakonov1992 <alexei.dyakonov@gmail.com>
2023-11-11 21:53:27 +0400

    feat: add data exchange between card and product-list

--- src/app/pages/products-list/card/card.component.ts
+++ src/app/pages/products-list/card/card.component.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {productsMock} from '../../../shared/products/products.mock';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {IProduct} from '../../../shared/products/product.interface';
 
 @Component({
     selector: 'app-card',
@@ -7,16 +7,18 @@ import {productsMock} from '../../../shared/products/products.mock';
     styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
-    readonly product = productsMock[0];
+    @Input() product?: IProduct;
+    @Output() readonly productBuy: EventEmitter<string> = new EventEmitter();
 
     onProductBuy(event: Event) {
         event.stopPropagation();
 
         // eslint-disable-next-line no-console
-        console.log('Buy product');
+        // console.log('Buy product from card');
+        this.productBuy.emit(this.product?._id);
     }
 
     isStarActive(starIndex: number): boolean {
-        return this.product.rating >= starIndex;
+        return this.product ? this.product.rating >= starIndex : false;
     }
 }
--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {IProduct} from '../../shared/products/product.interface';
+import {productsMock} from '../../shared/products/products.mock';
 
 @Component({
     selector: 'app-products-list',
@@ -6,8 +8,17 @@ import {Component} from '@angular/core';
     styleUrls: ['./products-list.component.css'],
 })
 export class ProductsListComponent {
+    readonly products?: IProduct[] = productsMock;
+    productTrash: Set<string> = new Set<string>();
+
     onCardClick() {
         // eslint-disable-next-line no-console
-        console.log('Card click');
+        // console.log('Card click');
+    }
+
+    onProductBuy(productId: string) {
+        this.productTrash.add(productId);
+        // eslint-disable-next-line no-console
+        console.log('Selected product ids: ', this.productTrash.values());
     }
 }
