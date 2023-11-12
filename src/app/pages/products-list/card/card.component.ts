2449ec50f3a72f247ae4e873bf3fe012a13f1090
danterep <antipoff7@yandex.ru>
2023-11-12 13:32:02 +0200

    feat: product card with notification

--- src/app/pages/products-list/card/card.component.ts
+++ src/app/pages/products-list/card/card.component.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {productsMock} from '../../../shared/products/products.mock';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {IProduct} from 'src/app/shared/products/product.interface';
 
 @Component({
     selector: 'app-card',
@@ -7,16 +7,22 @@ import {productsMock} from '../../../shared/products/products.mock';
     styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
-    readonly product = productsMock[0];
+    @Input() product: IProduct | undefined;
+    @Output() productAddedToCart = new EventEmitter<IProduct['_id']>();
 
     onProductBuy(event: Event) {
         event.stopPropagation();
 
-        // eslint-disable-next-line no-console
-        console.log('Buy product');
+        if (this.product) {
+            this.productAddedToCart.emit(this.product._id);
+        }
     }
 
     isStarActive(starIndex: number): boolean {
-        return this.product.rating >= starIndex;
+        if (this.product) {
+            return this.product.rating >= starIndex;
+        }
+
+        return false;
     }
 }
--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,4 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {IProduct} from 'src/app/shared/products/product.interface';
+import {productsMock} from 'src/app/shared/products/products.mock';
 
 @Component({
     selector: 'app-products-list',
@@ -6,8 +8,19 @@ import {Component} from '@angular/core';
     styleUrls: ['./products-list.component.css'],
 })
 export class ProductsListComponent {
+    @Input() products: IProduct[] = productsMock;
+
     onCardClick() {
         // eslint-disable-next-line no-console
         console.log('Card click');
+    }
+
+    onProductAddedToCart(event: IProduct['_id']) {
+        // eslint-disable-next-line no-alert
+        alert(`Product added to cart: ${event}`);
+    }
+
+    trackByProducts(index: number, product: IProduct): IProduct['_id'] {
+        return product._id;
     }
 }
