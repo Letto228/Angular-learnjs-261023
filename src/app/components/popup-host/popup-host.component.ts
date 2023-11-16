c428c0fe384adc73fb4bbfde33e3ad4e50b3db19
OneScriptMan <vladzam79@gmail.com>
2023-11-16 16:04:48 +0300

    feat: add popup component

---
+++ src/app/components/popup-host/popup-host.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-popup-host',
+    templateUrl: './popup-host.component.html',
+    styleUrls: ['./popup-host.component.css'],
+})
+export class PopupHostComponent {}
