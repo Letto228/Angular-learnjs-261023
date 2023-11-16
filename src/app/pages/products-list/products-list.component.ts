5a8cee795b7fcde4d85a391de3d5ea4fe82774aa
Letto228 <learnjs@MacBook-Pro-Egor.local>
2023-11-17 00:10:15 +0300

    feat: lecture 5 homework

--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {productsMock} from '../../shared/products/products.mock';
 import {IProduct} from '../../shared/products/product.interface';
+import {LoadDirection} from '../../shared/scroll-with-loading/load-direction.const';
 
 @Component({
     selector: 'app-products-list',
@@ -21,5 +22,10 @@ export class ProductsListComponent implements OnInit {
         setTimeout(() => {
             this.productsStore = productsMock;
         }, 4000);
+    }
+
+    onLoad(direction: LoadDirection) {
+        // eslint-disable-next-line no-console
+        console.log(`load ${direction}`);
     }
 }
