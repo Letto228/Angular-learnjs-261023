7d20cc61df1eefb1c6955c57335d6850241b2bf4
OneScriptMan <vladzam79@gmail.com>
2023-11-17 10:20:35 +0300

    feat: add popup fucntionality

--- src/app/components/popup-host/popup-host.component.ts
+++ src/app/components/popup-host/popup-host.component.ts
@@ -1,8 +1,29 @@
-import {Component} from '@angular/core';
+import {
+    Component,
+    Input,
+    TemplateRef,
+    ViewChild,
+    ViewContainerRef,
+    OnChanges,
+    OnInit,
+} from '@angular/core';
 
 @Component({
     selector: 'app-popup-host',
     templateUrl: './popup-host.component.html',
     styleUrls: ['./popup-host.component.css'],
 })
-export class PopupHostComponent {}
+export class PopupHostComponent implements OnChanges, OnInit {
+    @Input() template: TemplateRef<unknown> | null = null;
+
+    @ViewChild('viewPort', {static: true, read: ViewContainerRef})
+    private readonly viewPort?: ViewContainerRef | null = null;
+
+    ngOnChanges() {
+        this.viewPort?.clear();
+
+        if (this.template) {
+            this.viewPort?.createEmbeddedView(this.template);
+        }
+    }
+}
