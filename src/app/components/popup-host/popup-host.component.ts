823ea77778144a055f68b5a25c85ac8fc61bcedf
nikolaevfo <nikolaevfo@gmail.com>
2023-11-18 22:10:09 +0400

    feat: show popup

--- src/app/components/popup-host/popup-host.component.ts
+++ src/app/components/popup-host/popup-host.component.ts
@@ -1,8 +1,39 @@
-import {Component} from '@angular/core';
+import {
+    Component,
+    Input,
+    OnChanges,
+    SimpleChanges,
+    TemplateRef,
+    ViewChild,
+    ViewContainerRef,
+} from '@angular/core';
 
 @Component({
     selector: 'app-popup-host',
     templateUrl: './popup-host.component.html',
     styleUrls: ['./popup-host.component.css'],
 })
-export class PopupHostComponent {}
+export class PopupHostComponent implements OnChanges {
+    @Input() template: TemplateRef<unknown> | null = null;
+
+    @ViewChild('viewPort', {static: true, read: ViewContainerRef})
+    private readonly viewPort?: ViewContainerRef;
+
+    ngOnChanges({template}: SimpleChanges): void {
+        if (template) {
+            this.updateView();
+        }
+    }
+
+    private updateView() {
+        this.viewPort?.clear();
+
+        if (this.template) {
+            this.viewPort?.createEmbeddedView(this.template);
+        }
+    }
+
+    get isViewPortEmpty(): boolean {
+        return !this.viewPort?.length;
+    }
+}
