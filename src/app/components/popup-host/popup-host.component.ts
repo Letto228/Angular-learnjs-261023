002f04a0571caefeb1f2e6cc67770a17efc88880
OneScriptMan <146519340+OneScriptMan@users.noreply.github.com>
2023-11-21 11:10:50 +0300

    refactor: exclude method from ngOnChanges

--- src/app/components/popup-host/popup-host.component.ts
+++ src/app/components/popup-host/popup-host.component.ts
@@ -18,11 +18,17 @@ export class PopupHostComponent implements OnChanges{
     @ViewChild('viewPort', {static: true, read: ViewContainerRef})
     private readonly viewPort?: ViewContainerRef | null = null;
 
-    ngOnChanges({template}: SimpleChanges) {
-        this.viewPort?.clear();
-
-        if (this.template) {
-            this.viewPort?.createEmbeddedView(this.template);
-        }
+   ngOnChanges({template}: SimpleChanges): void {
+    if (template) {
+        this.updateView();
     }
+}
+
+private updateView() {
+    this.viewPort?.clear();
+
+    if (this.template) {
+        this.viewPort?.createEmbeddedView(this.template);
+    }
+}
 }
