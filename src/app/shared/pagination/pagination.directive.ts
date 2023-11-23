0527e0c75b0785541db8ea35640eb68738255e46
Dmitry Shmatok <dev@sevgeek.ru>
2023-11-23 16:47:21 +0300

    feat: добавляет директиву PaginationDirective

---
+++ src/app/shared/pagination/pagination.directive.ts
@@ -0,0 +1,122 @@
+/* eslint-disable no-console */
+
+import {
+    Input,
+    Directive,
+    OnChanges,
+    TemplateRef,
+    SimpleChanges,
+    ViewContainerRef,
+} from '@angular/core';
+
+type PaginationDirectiveContext<T> = {
+    $implicit: T[];
+    index: number;
+    pageIndexes: number[];
+
+    next: VoidFunction;
+    back: VoidFunction;
+    selectIndex: (index: number) => void;
+};
+
+const splitArrayIntoChunks = <T>(value: T[], chunkSize: number): T[][] => {
+    const result = [];
+
+    while (value.length) {
+        const chunk = value.splice(0, chunkSize);
+
+        result.push(chunk);
+    }
+
+    return result;
+};
+
+@Directive({
+    selector: '[appPagination]',
+})
+export class PaginationDirective<T> implements OnChanges {
+    @Input()
+    appPaginationOf: T[] = [];
+
+    @Input()
+    appPaginationChankSize = 1;
+
+    private index = 0;
+    private pageIndexes: number[] = [];
+    private paginationChunks: T[][] = [];
+
+    constructor(
+        private readonly template: TemplateRef<unknown>,
+        private readonly viewContainer: ViewContainerRef,
+    ) {}
+
+    ngOnChanges({appPaginationOf, appPaginationChankSize}: SimpleChanges) {
+        if (appPaginationOf && appPaginationChankSize) {
+            this.updatePaginationChunks();
+            this.updateViewContainer();
+        }
+    }
+
+    private updatePaginationChunks() {
+        this.index = 0;
+
+        this.paginationChunks = splitArrayIntoChunks(
+            this.appPaginationOf,
+            this.appPaginationChankSize,
+        );
+
+        this.pageIndexes = Array.from(this.paginationChunks.keys());
+    }
+
+    next() {
+        const nextIndex = this.index + 1;
+
+        if (this.pageIndexes.includes(nextIndex)) {
+            this.index = nextIndex;
+            this.updateViewContainer();
+        }
+    }
+
+    back() {
+        const prevIndex = this.index - 1;
+
+        if (this.pageIndexes.includes(prevIndex)) {
+            this.index = prevIndex;
+            this.updateViewContainer();
+        }
+    }
+
+    private setIndex(index: number) {
+        if (index === this.index || !this.pageIndexes.includes(index)) {
+            return;
+        }
+
+        this.index = index;
+        this.updateViewContainer();
+    }
+
+    private updateViewContainer() {
+        this.viewContainer.clear();
+        this.viewContainer.createEmbeddedView(this.template, this.getDirectiveContext());
+    }
+
+    private getDirectiveContext(): PaginationDirectiveContext<T> {
+        const $implicit = this.paginationChunks[this.index];
+
+        return {
+            $implicit,
+            index: this.index,
+            pageIndexes: this.pageIndexes,
+            next: () => this.next(),
+            back: () => this.back(),
+            selectIndex: (index: number) => this.setIndex(index),
+        };
+    }
+
+    static ngTemplateContextGuard<T>(
+        _directive: PaginationDirective<T>,
+        context: PaginationDirectiveContext<T>,
+    ): context is PaginationDirectiveContext<T> {
+        return true;
+    }
+}
