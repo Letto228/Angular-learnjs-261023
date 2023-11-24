c3df2b186b9f6ef89a242c0a227c7411901ae498
AlekseyDyakonov1992 <alexei.dyakonov@gmail.com>
2023-11-24 20:56:03 +0400

    fix: correct after review

--- src/app/shared/scroll-with-loading/scroll-with-loading.directive.ts
+++ src/app/shared/scroll-with-loading/scroll-with-loading.directive.ts
@@ -1,32 +1,32 @@
-import {Directive, HostListener, Output} from '@angular/core';
-import {Subject} from 'rxjs';
+import {Directive, EventEmitter, HostListener, Output} from '@angular/core';
 import {LoadDirection} from './load-direction';
 
 @Directive({
     selector: '[appScrollWithLoading]',
 })
 export class ScrollWithLoadingDirective {
-    @Output() readonly loadData = new Subject<LoadDirection>();
+    @Output() readonly loadData = new EventEmitter<LoadDirection>();
 
     private prevScrollTop = -1;
     private readonly borderOffset = 100;
 
     @HostListener('scroll', ['$event.target'])
     onScroll({scrollTop, clientHeight, scrollHeight}: HTMLElement) {
-        this.prevScrollTop = scrollTop;
-        setTimeout(() => {
-            if (this.isLoadUp(scrollTop, this.prevScrollTop)) {
-                this.loadData.next(LoadDirection.UP);
-            }
+        const prevScrollTop = this.prevScrollTop;
 
-            if (this.isLoadDown(scrollTop, this.prevScrollTop, clientHeight, scrollHeight)) {
-                this.loadData.next(LoadDirection.DOWN);
-            }
-        }, 50);
+        this.prevScrollTop = scrollTop;
+
+        if (this.isLoadUp(scrollTop, prevScrollTop)) {
+            this.loadData.emit(LoadDirection.UP);
+        }
+
+        if (this.isLoadDown(scrollTop, prevScrollTop, clientHeight, scrollHeight)) {
+            this.loadData.emit(LoadDirection.DOWN);
+        }
     }
 
     private isLoadUp(scrollTop: number, prevScrollTop: number) {
-        return scrollTop < this.borderOffset && scrollTop > prevScrollTop;
+        return prevScrollTop < this.borderOffset && scrollTop <= prevScrollTop;
     }
 
     private isLoadDown(
@@ -37,7 +37,7 @@ export class ScrollWithLoadingDirective {
     ) {
         return (
             clientHeight + scrollTop >= scrollHeight - this.borderOffset &&
-            scrollTop < prevScrollTop
+            scrollTop >= prevScrollTop
         );
     }
 }
