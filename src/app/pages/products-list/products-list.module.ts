f88ebb771032b41d0f47053c9c57b0f138f3c169
a.ukolov <a.ukolov@bftcom.com>
2023-11-25 16:25:32 +0300

    feat: реализовал пагинацию

--- src/app/pages/products-list/products-list.module.ts
+++ src/app/pages/products-list/products-list.module.ts
@@ -4,10 +4,12 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {ProductsListComponent} from './products-list.component';
 import {CardModule} from './card/card.module';
 import {IfModule} from '../../shared/if/if.module';
+import {MatIconModule} from "@angular/material/icon";
+import {PaginationModule} from "../../shared/pagination/pagination.module";
 
 @NgModule({
     declarations: [ProductsListComponent],
-    imports: [CardModule, IfModule, CommonModule, MatProgressSpinnerModule],
+    imports: [CardModule, IfModule, MatIconModule, CommonModule, MatProgressSpinnerModule, PaginationModule],
     exports: [ProductsListComponent],
 })
 export class ProductsListModule {}
