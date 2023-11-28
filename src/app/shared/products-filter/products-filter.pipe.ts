9bdb2d7c57c68bb6bb0033d4d853e8be8b2a796d
Eugene <kotlyarov.evgeny@gmail.com>
2023-11-28 05:22:26 +0400

    refactor: check for search strings

--- src/app/shared/products-filter/products-filter.pipe.ts
+++ src/app/shared/products-filter/products-filter.pipe.ts
@@ -7,27 +7,33 @@ export class ProductsFilterPipe implements PipeTransform {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     transform<T extends Record<string, any>, K extends keyof T>(
         entities?: T[],
-        field?: K,
+        fieldName?: K,
         match?: T[K],
+        caseSensitive?: boolean,
     ): T[] | null {
         if (!entities) {
             return null;
         }
 
-        if (!field) {
+        if (!fieldName) {
             return entities;
         }
 
         const filtered: T[] = [];
 
         entities.forEach(entity => {
-            if (
-                typeof entity[field] === 'string' &&
-                field === 'name' &&
-                (<string>entity[field]).includes(<string>match)
-            ) {
-                filtered.push(entity);
-            } else if (entity[field] === match) {
+            if (typeof entity[fieldName] === 'string') {
+                const fieldValue = caseSensitive
+                    ? <string>entity[fieldName]
+                    : (<string>entity[fieldName]).toLocaleLowerCase();
+                const searchValue = caseSensitive
+                    ? <string>match
+                    : (<string>match).toLocaleLowerCase();
+
+                if (fieldValue.includes(searchValue)) {
+                    filtered.push(entity);
+                }
+            } else if (entity[fieldName] === match) {
                 filtered.push(entity);
             }
         });
