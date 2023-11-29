de8a53304e0f51fe3a1b80e340d32df1dfc96d5b
Yuriy <yura.yuzgin@yandex.ru>
2023-11-29 19:27:58 +0300

    feat: add products-filter pipe

--- src/app/pages/products-list/products-list.module.ts
+++ src/app/pages/products-list/products-list.module.ts
@@ -3,6 +3,7 @@ import {CommonModule} from '@angular/common';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {ProductsFilterModule} from 'src/app/shared/products-filter/products-filter.module';
 import {ProductsListComponent} from './products-list.component';
 import {CardModule} from './card/card.module';
 import {IfModule} from '../../shared/if/if.module';
@@ -20,6 +21,7 @@ import {MyAsyncModule} from '../../shared/my-async/my-async.module';
         MatIconModule,
         PaginationModule,
         MyAsyncModule,
+        ProductsFilterModule,
     ],
     exports: [ProductsListComponent],
 })
