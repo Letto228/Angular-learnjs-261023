f8d0e509c3c20e96669e51048b1390a48f94b1ec
Igor Gritsenko <ctocopokcemb147@yandex.ru>
2023-11-30 01:35:14 +0300

    feat: lecture-6-dz

--- src/app/pages/products-list/products-list.module.ts
+++ src/app/pages/products-list/products-list.module.ts
@@ -1,13 +1,24 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 import {ProductsListComponent} from './products-list.component';
 import {CardModule} from './card/card.module';
 import {IfModule} from '../../shared/if/if.module';
+import {PaginationModule} from '../../shared/pagination/pagination.module';
 
 @NgModule({
     declarations: [ProductsListComponent],
-    imports: [CardModule, IfModule, CommonModule, MatProgressSpinnerModule],
+    imports: [
+        CardModule,
+        IfModule,
+        CommonModule,
+        MatProgressSpinnerModule,
+        MatButtonModule,
+        MatIconModule,
+        PaginationModule,
+    ],
     exports: [ProductsListComponent],
 })
 export class ProductsListModule {}
---
+++ src/app/shared/pagination/pagination.directive.ts
@@ -0,0 +1,100 @@
+import {
+    Directive,
+    Input,
+    OnChanges,
+    OnInit,
+    SimpleChanges,
+    TemplateRef,
+    ViewContainerRef,
+} from '@angular/core';
+import {BehaviorSubject, map} from 'rxjs';
+import {IPaginationContext} from './pagination-context.interface';
+
+@Directive({
+    selector: '[appPagination]',
+})
+export class PaginationDirective<T> implements OnChanges, OnInit {
+    @Input() appPaginationOf: T[] | undefined | null;
+    @Input() appPaginationChankSize = 4;
+
+    private groupedItems: Array<T>[] = [];
+
+    private readonly currentIndex$ = new BehaviorSubject<number>(0);
+
+    constructor(
+        private readonly viewContainerRef: ViewContainerRef,
+        private readonly templateRef: TemplateRef<IPaginationContext<T>>,
+    ) {}
+
+    ngOnChanges({appPaginationOf, appPaginationChankSize}: SimpleChanges) {
+        if (appPaginationOf || appPaginationChankSize) {
+            this.updateView();
+        }
+    }
+
+    ngOnInit() {
+        this.listenCurrentIndex();
+    }
+
+    private updateView() {
+        const shouldShowItems = this.appPaginationOf?.length;
+
+        if (shouldShowItems) {
+            this.viewContainerRef.clear();
+        }
+
+        for (let i = 0; i < this.appPaginationOf!.length; i += this.appPaginationChankSize) {
+            const chunk = this.appPaginationOf!.slice(i, i + this.appPaginationChankSize);
+            this.groupedItems.push(chunk);
+        }
+        this.currentIndex$.next(0);
+    }
+
+    private listenCurrentIndex() {
+        this.currentIndex$
+            .pipe(map(currentIndex => this.getCurrentContext(currentIndex)))
+            .subscribe(context => {
+                this.viewContainerRef.clear();
+                this.viewContainerRef.createEmbeddedView(this.templateRef, context);
+            });
+    }
+
+    private getCurrentContext(currentIndex: number): IPaginationContext<T> {
+        return {
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            $implicit: this.groupedItems[currentIndex],
+            index: currentIndex,
+            pageIndexes: this.groupedItems.map((item, index) => index),
+            appPaginationOf: this.appPaginationOf!,
+            next: () => {
+                this.next();
+            },
+            back: () => {
+                this.back();
+            },
+            selectIndex: (index: number) => {
+                this.selectIndex(index);
+            },
+        };
+    }
+
+    private next() {
+        const nextIndex = this.currentIndex$.value + 1;
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        const newIndex = nextIndex < this.groupedItems.length ? nextIndex : 0;
+
+        this.currentIndex$.next(newIndex);
+    }
+
+    private back() {
+        const previousIndex = this.currentIndex$.value - 1;
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        const newIndex = previousIndex >= 0 ? previousIndex : this.groupedItems.length - 1;
+
+        this.currentIndex$.next(newIndex);
+    }
+
+    private selectIndex(index: number) {
+        this.currentIndex$.next(index);
+    }
+}
